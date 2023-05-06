17be6a9f93d5a44dad52031a0b93917f331bae25
mahmoudSs7 <132747436+mahmoudSs7@users.noreply.github.com>
2023-05-06 12:41:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let counter = 0;
+let counter1 = 0;
+function add() {
+counter = counter +1 ;
+const para = document.getElementById("num");
+para.innerText = counter;	
+
+counter1 = document.getElementById("cost").innerHTML;
+const para2 = document.getElementById("co");
+para2.innerText = counter1;
+}
